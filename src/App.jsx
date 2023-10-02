2a10ad5a7c795370ab5f816b7e38769e77b154af
Sujal Thomas Tatipelli <42236789+sujalthomas@users.noreply.github.com>
2023-10-01 19:38:36 -0700

    -analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter } from "react-router-dom";
+import { Analytics } from '@vercel/analytics/react';
 
 import { About, Contact, Experience, Feedbacks, Hero, Navbar, Works, StarsCanvas, } from "./components";
 
@@ -21,7 +22,7 @@ const App = () => {
           <StarsCanvas />
         </div>
 
-
+        <Analytics />
       </div>
 
     </BrowserRouter>
