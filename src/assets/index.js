bdfbea95a1aad126c70be97137c8359a6d11b375
Sujal Thomas Tatipelli <42236789+sujalthomas@users.noreply.github.com>
2023-10-09 22:19:47 -0700

    tree-shake

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,5 +1,4 @@
 import logo from "./logo.svg";
-import github from "./github.png";
 import menu from "./menu.svg";
 import close from "./close.svg";
 
@@ -7,7 +6,6 @@ import close from "./close.svg";
 
 export {
   logo,
-  github,
   menu,
   close,
 };
