1215282e06bbd52da46da46c21eb435fb66d0dc0
Sujal Thomas Tatipelli <42236789+sujalthomas@users.noreply.github.com>
2023-10-09 22:43:41 -0700

    ab

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Tilt from "react-tilt";
 import { motion } from "framer-motion";
 
@@ -7,38 +7,61 @@ import { services } from "../constants";
 import { SectionWrapper } from "../hoc";
 import { fadeIn, textVariant } from "../utils/motion";
 
-const ServiceCard = ({ index, title, description }) => (
-  <Tilt className='xs:w-[250px] w-full'>
-    <motion.div
-      variants={fadeIn("right", "spring", index * 0.05, 0.15)}
-      className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'
-    >
-      <div
-        options={{
-          max: 45,
-          scale: 1,
-          speed: 450,
-        }}
-        className='bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col'
-      >
-        <h3 className='text-cyan-200 text-[20px] font-bold text-center'>
-          {title}
-        </h3>
-        <h4 className='text-cyan-100 text-[15px] font-semibold text-center pt-5'>
-          {description}
-        </h4>
-      </div>
-    </motion.div>
-  </Tilt>
-);
+const ServiceCard = ({ index, title, description }) => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    setIsMobile(window.innerWidth <= 768);
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 768);
+    };
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  return (
+    <>
+      {isMobile ? (
+        <div className='xs:w-[250px] w-full'>
+          <div className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'>
+            <div className='bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col'>
+              <h3 className='text-cyan-200 text-[20px] font-bold text-center'>
+                {title}
+              </h3>
+              <h4 className='text-cyan-100 text-[15px] font-semibold text-center pt-5'>
+                {description}
+              </h4>
+            </div>
+          </div>
+        </div>
+      ) : (
+        <Tilt className='xs:w-[250px] w-full'>
+          <motion.div
+            variants={fadeIn("right", "spring", index * 0.05, 0.15)}
+            className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'
+          >
+            <div className='bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col'>
+              <h3 className='text-cyan-200 text-[20px] font-bold text-center'>
+                {title}
+              </h3>
+              <h4 className='text-cyan-100 text-[15px] font-semibold text-center pt-5'>
+                {description}
+              </h4>
+            </div>
+          </motion.div>
+        </Tilt>
+      )}
+    </>
+  );
+};
 
 const About = () => {
   return (
     <>
-
-        <p className={styles.sectionSubText}>Introduction</p>
-        <h2 className={styles.sectionHeadText}>Overview</h2>
-
+      <p className={styles.sectionSubText}>Introduction</p>
+      <h2 className={styles.sectionHeadText}>Overview</h2>
 
       <motion.p
         variants={fadeIn("", "", 0.1, 1)}
